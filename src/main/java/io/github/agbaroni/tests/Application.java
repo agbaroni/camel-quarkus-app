01c240236f0f1051e6648b4d99fe87fc65b91041
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2022-03-25 14:24:56 +0100

    Initial commit

---
+++ src/main/java/io/github/agbaroni/tests/Application.java
@@ -0,0 +1,10 @@
+package io.github.agbaroni.tests;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class Application extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+    }
+}
